e1b118857eb9cd98b0d1abf3d59f26b44d303310
Leaf217 <leaf_217@163.com>
2018-01-04 22:57:40 +0800

    v0104

--- script/data.js
+++ script/data.js
@@ -6,24 +6,24 @@
 localStorage.clear(); //清除
 //JSON对象
 var cards = [
-    {
-        "index": 0,
-        "title": "关于float的那些事儿",
-        "URL": "http://www.w3school.com.cn/cssref/pr_class_float.asp",
-        "progress": 100,
-        "evaluation": 3,
-        "notes": "关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿",
-        "tags": ['Tag1', 'Tag2', 'Tag3']
-    },
-    {
-        "index": 1,
-        "title": "position知多少",
-        "URL": "http://www.w3school.com.cn/cssref/pr_class_position.asp",
-        "progress": 75,
-        "evaluation": 4,
-        "notes": "position知多少position知多少position知多少position知多少position知多少position知多少position知多少position知多少position知多少",
-        "tags": ['Tag1', 'Tag2']
-    }
+    // {
+    //     "index": 0,
+    //     "title": "关于float的那些事儿",
+    //     "URL": "http://www.w3school.com.cn/cssref/pr_class_float.asp",
+    //     "progress": 100,
+    //     "evaluation": 3,
+    //     "notes": "关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿关于float的那些事儿",
+    //     "tags": ['Tag1', 'Tag2', 'Tag3']
+    // },
+    // {
+    //     "index": 1,
+    //     "title": "position知多少",
+    //     "URL": "http://www.w3school.com.cn/cssref/pr_class_position.asp",
+    //     "progress": 75,
+    //     "evaluation": 4,
+    //     "notes": "position知多少position知多少position知多少position知多少position知多少position知多少position知多少position知多少position知多少",
+    //     "tags": ['Tag1', 'Tag2']
+    // }
 ]
 
 cards = JSON.stringify(cards); //将JSON对象转化成字符串
